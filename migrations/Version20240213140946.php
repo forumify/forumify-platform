6690513f70220375b3153b39fd7b50ba3ecf569f
BuildTools <unconfigured@null.spigotmc.org>
2024-02-13 14:24:29 +0000

    Removed comments, added description

--- migrations/Version20240213140946.php
+++ migrations/Version20240213140946.php
@@ -7,25 +7,21 @@ namespace ForumifyDoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
+
 final class Version20240213140946 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Makes settings value nullable';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE setting CHANGE value value JSON DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE setting CHANGE value value JSON NOT NULL');
     }
 }
