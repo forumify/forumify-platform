6b4cb9d585e3597115e6650e0a75613ef7112540
jannes <jannesdrijkoningen@gmail.com>
2024-03-02 20:14:36 +0100

    updated forumify

--- src/Core/Twig/Extension/RichTextExtension.php
+++ src/Core/Twig/Extension/RichTextExtension.php
@@ -9,6 +9,37 @@ use Twig\TwigFilter;
 
 class RichTextExtension extends AbstractExtension
 {
+    private const TAG_WHITELIST = [
+        'h1',
+        'h2',
+        'h3',
+        'h4',
+        'h5',
+        'h6',
+        'div',
+        'p',
+        'span',
+        'br',
+        'a',
+        'img',
+        'strong',
+        'em',
+        'blockquote',
+        'code',
+        'pre',
+        'ul',
+        'ol',
+        'li',
+    ];
+
+    private const ATTR_WHITELIST = [
+        'class',
+        'style',
+        'href',
+        'src',
+        'alt',
+    ];
+
     public function getFilters(): array
     {
         return [
