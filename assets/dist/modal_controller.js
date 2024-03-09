d6397ef5951dfe54aab0944147b34f6f1e790d39
jannes <jannesdrijkoningen@gmail.com>
2024-03-09 23:21:20 +0100

    add modal component

---
+++ assets/dist/modal_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+  static targets = ['modal', 'modalBody'];
+
+  connect() {
+    this.element.firstElementChild.addEventListener('click', this.open.bind(this));
+
+    // click-away listener
+    this.modalTarget.addEventListener('click', (e) => {
+      if (!this.modalBodyTarget.contains(e.target)) {
+        this.close();
+      }
+    });
+  }
+
+  open() {
+    this.modalTarget.classList.add('open');
+  }
+
+  close() {
+    this.modalTarget.classList.remove('open');
+  }
+}
