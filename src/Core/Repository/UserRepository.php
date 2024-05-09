8146d502c7d42bd8dfc07c774cdf4ced7503a5a4
Oscar <oscarx1be@gmail.com>
2024-05-09 18:33:37 +0100

    Issue #8: add login by email, username, or both.

--- src/Core/Repository/UserRepository.php
+++ src/Core/Repository/UserRepository.php
@@ -2,12 +2,41 @@
 
 namespace Forumify\Core\Repository;
 
+use Doctrine\Persistence\ManagerRegistry;
 use Forumify\Core\Entity\User;
+use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
 
-class UserRepository extends AbstractRepository
+class UserRepository extends AbstractRepository implements UserLoaderInterface
 {
+    private $settingRepository;
+
+    public function __construct(ManagerRegistry $registry, SettingRepository $settingRepository)
+    {
+        parent::__construct($registry);
+        $this->settingRepository = $settingRepository;
+    }
+
     public static function getEntityClass(): string
     {
         return User::class;
+    }
+
+    public function loadUserByIdentifier(string $usernameOrEmail): ?User
+    {
+        $queryBuilder = $this->createQueryBuilder('u');
+
+        $where = match($this->settingRepository->get('core.enable_email_login')) {
+            'email' => 'u.email = :query',
+            'both' => 'u.username = :query OR u.email = :query',
+            default => 'u.username = :query',
+        };
+        $queryBuilder->where($where);
+
+        return $this->createQueryBuilder('u')
+            ->where($where)
+            ->setParameter('query', $usernameOrEmail)
+            ->getQuery()
+            ->getOneOrNullResult();
+
     }
 }
