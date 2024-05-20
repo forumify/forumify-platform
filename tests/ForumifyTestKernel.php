6e45017bf5eb1dcdd523d5d1093819cb241f1fbb
jannes <jannesdrijkoningen@gmail.com>
2024-05-20 14:28:28 +0200

    add test setup

---
+++ tests/ForumifyTestKernel.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use Forumify\Core\ForumifyKernel;
+
+class ForumifyTestKernel extends ForumifyKernel
+{
+    public function __construct(string $env, bool $debug = false)
+    {
+        $context = ['APP_ENV' => $env, 'APP_DEBUG' => $debug];
+        parent::__construct($context, dirname(__DIR__) . '/tests');
+    }
+
+    public function registerBundles(): iterable
+    {
+        return [
+            new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new \Forumify\ForumifyBundle(),
+            new \Symfony\Bundle\TwigBundle\TwigBundle(),
+            new \Twig\Extra\TwigExtraBundle\TwigExtraBundle(),
+            new \Symfony\WebpackEncoreBundle\WebpackEncoreBundle(),
+            new \Symfony\UX\TwigComponent\TwigComponentBundle(),
+            new \Symfony\UX\LiveComponent\LiveComponentBundle(),
+            new \Symfony\UX\Autocomplete\AutocompleteBundle(),
+            new \Symfony\UX\StimulusBundle\StimulusBundle(),
+            new \Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new \Symfony\Bundle\MonologBundle\MonologBundle(),
+            new \Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new \League\FlysystemBundle\FlysystemBundle(),
+            new \Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new \Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new \DAMA\DoctrineTestBundle\DAMADoctrineTestBundle(),
+        ];
+    }
+}
---
+++ tests/Tests/Application/Core/IndexControllerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Tests\Application\Core;
+
+use Forumify\Cms\Entity\Page;
+use Forumify\Cms\Repository\PageRepository;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class IndexControllerTest extends WebTestCase
+{
+    public function testIndex(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+
+        self::assertResponseIsSuccessful();
+        self::assertSelectorTextContains('h1', 'Welcome to forumify!');
+    }
+
+    public function testIndexPage(): void
+    {
+        $client = static::createClient();
+
+        $pageRepository = self::getContainer()->get(PageRepository::class);
+        $page = new Page();
+        $page->setUrlKey('');
+        $page->setTitle('Index');
+        $page->setTwig('<h1>Welcome from pages!</h1>');
+        $pageRepository->save($page);
+
+        $client->request('GET', '/');
+
+        self::assertResponseIsSuccessful();
+        self::assertSelectorTextContains('h1', 'Welcome from pages!');
+    }
+}
