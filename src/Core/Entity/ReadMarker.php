6f1311977c903012969f170430b2fd4a55b97386
jannes <jannesdrijkoningen@gmail.com>
2024-09-01 18:55:38 +0200

    fix missing read markers class

---
+++ src/Core/Entity/ReadMarker.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Forumify\Core\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Forumify\Core\Repository\ReadMarkerRepository;
+
+#[ORM\Entity(repositoryClass: ReadMarkerRepository::class)]
+class ReadMarker
+{
+    #[ORM\Id]
+    #[ORM\ManyToOne(User::class)]
+    #[ORM\JoinColumn(onDelete: 'CASCADE')]
+    private User $user;
+
+    #[ORM\Id]
+    #[ORM\Column]
+    private string $subject;
+
+    #[ORM\Id]
+    #[ORM\Column(type: 'integer')]
+    private int $subjectId;
+
+    public function __construct(User $user, string $subject, int $subjectId) {
+        $this->user = $user;
+        $this->subject = $subject;
+        $this->subjectId = $subjectId;
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    public function getSubject(): string
+    {
+        return $this->subject;
+    }
+
+    public function getSubjectId(): int
+    {
+        return $this->subjectId;
+    }
+}
