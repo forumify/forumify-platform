7caee2dffa5718326fb45a6d1286a069ce512283
jannes <jannesdrijkoningen@gmail.com>
2024-09-19 21:48:13 +0200

    add dark mode to admin

--- assets/dist/code_editor_controller.js
+++ assets/dist/code_editor_controller.js
@@ -5,6 +5,7 @@ import 'ace-builds/src-noconflict/mode-html';
 import 'ace-builds/src-noconflict/mode-javascript';
 import 'ace-builds/src-noconflict/mode-twig';
 import 'ace-builds/src-noconflict/theme-github';
+import 'ace-builds/src-noconflict/theme-github_dark';
 import 'ace-builds/src-noconflict/ext-language_tools';
 
 export default class extends Controller {
@@ -16,8 +17,16 @@ export default class extends Controller {
   }
 
   connect() {
+    const preferredTheme = theme === 'system'
+      ? (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'default')
+      : theme;
+
+    const defaultTheme = preferredTheme === 'default'
+      ? 'ace/theme/github'
+      : 'ace/theme/github_dark';
+
     const editor = ace.edit(this.editorIdValue, {
-      theme: this.themeValue || 'ace/theme/github',
+      theme: this.themeValue || defaultTheme,
       value: this.valueValue || '',
       autoScrollEditorIntoView: true,
       maxLines: 40,
