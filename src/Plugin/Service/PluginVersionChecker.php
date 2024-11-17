e8bef07c9de11bf0e3467aa5c4da32ea70389902
jannes <jannesdrijkoningen@gmail.com>
2024-11-17 13:48:34 +0100

    add indicator when a field is optional

--- src/Plugin/Service/PluginVersionChecker.php
+++ src/Plugin/Service/PluginVersionChecker.php
@@ -5,16 +5,24 @@ declare(strict_types=1);
 namespace Forumify\Plugin\Service;
 
 use Forumify\Core\Repository\PluginRepository;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 class PluginVersionChecker
 {
     public function __construct(
         private readonly PluginRepository $pluginRepository,
+        #[Autowire('%kernel.environment%')]
+        private readonly string $env,
     ) {
     }
 
     public function isVersionInstalled(string $pluginPackage, string|array $versions): bool
     {
+        if (in_array($this->env, ['dev', 'test'])) {
+            // no checks in unit tests or dev mode
+            return true;
+        }
+
         if (is_string($versions)) {
             $versions = [$versions];
         }
