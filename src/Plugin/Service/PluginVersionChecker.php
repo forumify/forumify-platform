6a61663437aca83e98c31dd8979feb64f7d17135
jannes <jannesdrijkoningen@gmail.com>
2024-11-30 18:39:36 +0100

    allow no version to be passed to plugin version checker

--- src/Plugin/Service/PluginVersionChecker.php
+++ src/Plugin/Service/PluginVersionChecker.php
@@ -9,8 +9,6 @@ use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 class PluginVersionChecker
 {
-    private array $installedVersionCache = [];
-
     public function __construct(
         private readonly PluginRepository $pluginRepository,
         #[Autowire('%kernel.environment%')]
@@ -18,28 +16,21 @@ class PluginVersionChecker
     ) {
     }
 
-    public function isVersionInstalled(string $pluginPackage, string|array $versions): bool
+    public function isVersionInstalled(string $pluginPackage, string|array $versions = []): bool
     {
-        if (in_array($this->env, ['dev', 'test'])) {
-            // no checks in unit tests or dev mode
-            return true;
-        }
-
         if (is_string($versions)) {
             $versions = [$versions];
         }
 
-        if (isset($this->installedVersionCache[$pluginPackage])) {
-            return $this->installedVersionCache[$pluginPackage];
-        }
-
-        $plugin = $this->pluginRepository->findOneBy(['package' => $pluginPackage]);
+        $plugin = $this->pluginRepository->findOneBy(['package' => $pluginPackage, 'active' => true]);
         if ($plugin === null) {
-            $this->installedVersionCache[$pluginPackage] = false;
             return false;
         }
 
-        $this->installedVersionCache[$pluginPackage] = in_array($plugin->getSubscriptionVersion(), $versions, true);
-        return $this->installedVersionCache[$pluginPackage];
+        if (empty($versions) || in_array($this->env, ['dev', 'test'])) {
+            return true;
+        }
+
+        return in_array($plugin->getSubscriptionVersion(), $versions, true);
     }
 }
