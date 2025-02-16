bc86cfd6cabe33df54c091d8bb0e40fb6b8575b9
jannes <jannesdrijkoningen@gmail.com>
2025-02-16 15:21:20 +0100

    set twig timezone globally
    closes #65

---
+++ src/Core/EventSubscriber/SetTwigTimezoneListener.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Forumify\Core\EventSubscriber;
+
+use Forumify\Core\Entity\User;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Twig\Environment;
+use Twig\Extension\CoreExtension;
+
+#[AsEventListener(KernelEvents::REQUEST)]
+class SetTwigTimezoneListener
+{
+    public function __construct(
+        private readonly Security $security,
+        private readonly Environment $twig
+    ) {
+    }
+
+    public function __invoke(): void
+    {
+        $user = $this->security->getUser();
+        if ($user instanceof User) {
+            $coreExt = $this->twig->getExtension(CoreExtension::class);
+            $coreExt->setTimezone($user->getTimezone());
+        }
+    }
+}
