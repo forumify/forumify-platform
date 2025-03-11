c307084aabac583cb57e3e7c7ad57e05d6a9dacc
jannes <jannesdrijkoningen@gmail.com>
2025-03-11 22:46:45 +0100

    fix doctrine automations being fetched for repeated operations

--- src/Automation/Trigger/AbstractDoctrineTrigger.php
+++ src/Automation/Trigger/AbstractDoctrineTrigger.php
@@ -4,12 +4,18 @@ declare(strict_types=1);
 
 namespace Forumify\Automation\Trigger;
 
+use Forumify\Automation\Entity\Automation;
 use Forumify\Automation\Form\DoctrineTriggerType;
 use Forumify\Automation\Repository\AutomationRepository;
 use Forumify\Automation\Scheduler\AutomationScheduler;
 
 abstract class AbstractDoctrineTrigger
 {
+    /**
+     * @var null|array<Automation>
+     */
+    private ?array $automations = null;
+
     public function __construct(
         private readonly AutomationRepository $automationRepository,
         private readonly AutomationScheduler $automationScheduler,
@@ -25,8 +31,8 @@ abstract class AbstractDoctrineTrigger
 
     protected function trigger(object $entity): void
     {
-        $automations = $this->automationRepository->findByTriggerType(static::getType());
-        foreach ($automations as $automation) {
+        $this->automations ??= $this->automationRepository->findByTriggerType(static::getType());
+        foreach ($this->automations as $automation) {
             $entities = $automation->getTriggerArguments()['entities'] ?? [];
             if (empty($entities) || in_array(get_class($entity), $entities, true)) {
                 $this->automationScheduler->schedule($automation, ['entity' => $entity]);
