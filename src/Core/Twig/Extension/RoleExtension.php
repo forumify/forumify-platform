1d14783fc2ff65d9774c1d27bd5f080f8bf1f04d
Oscar Ellis <oscarx1be@gmail.com>
2025-05-07 20:04:36 +0100

    code quality to PSR12 standards

--- src/Core/Twig/Extension/RoleExtension.php
+++ src/Core/Twig/Extension/RoleExtension.php
@@ -6,29 +6,23 @@ namespace Forumify\Core\Twig\Extension;
 
 use Forumify\Core\Entity\Role;
 use Forumify\Core\Entity\User;
-use Forumify\Core\Repository\RoleRepository;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 
 class RoleExtension extends AbstractExtension
 {
-    public function __construct(
-        private RoleRepository $roleRepository
-    ){}
     public function getFilters(): array
     {
         return [
             new TwigFilter('role_color', [$this, 'roleColor']),
         ];
     }
+
     public function roleColor(?User $user): ?string
     {
         $role = $user
             ?->getRoleEntities()
-            ?->filter(fn(Role $r) =>
-                $r->isShowOnUsername()
-                && $r->getColor() !== '#000000'
-            )
+            ?->filter(fn(Role $r) => $r->isShowOnUsername() && $r->getColor() !== '#000000')
             ?->first();
 
         return $role ? $role->getColor() : null;
