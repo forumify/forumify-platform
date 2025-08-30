33e0130955d7fee176d326d715000e22bd6e5a45
jannes <jannesdrijkoningen@gmail.com>
2025-08-30 12:07:26 +0200

    add messenger improvements

---
+++ assets/src/controllers/frontend/Messenger.js
@@ -0,0 +1,5 @@
+import { Controller } from '@hotwired/stimulus';
+
+export class Messenger extends Controller {
+
+}
--- assets/src/entrypoints/frontend.js
+++ assets/src/entrypoints/frontend.js
@@ -2,9 +2,10 @@ import '../styles/frontend/index.scss'
 import framework from './framework';
 import { CommentEditor } from '../controllers/frontend/CommentEditor';
 import { ProfilePreview } from '../controllers/frontend/ProfilePreview';
-
+import { Messenger } from '../controllers/frontend/Messenger';
 
 const register = framework();
 
 register('comment-editor', CommentEditor);
 register('profile-preview', ProfilePreview);
+register('messenger', Messenger);
