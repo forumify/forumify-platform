06bf718c9031d428f89e5727c532f5cc7fb4ebeb
jannes <jannesdrijkoningen@gmail.com>
2026-04-17 17:39:57 +0200

    fix forgot password
    fixes #156

---
+++ tests/Tests/Application/Core/ForgotPasswordControllerTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Core;
+
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Tests\Tests\Traits\UserTrait;
+
+class ForgotPasswordControllerTest extends WebTestCase
+{
+    use UserTrait;
+
+    public function testForgotPasswordUserNotFound(): void
+    {
+        $client = static::createClient();
+
+        $this->runTest($client, 'blipblop');
+    }
+
+    public function testForgotPasswordUserFound(): void
+    {
+        $client = static::createClient();
+
+        $this->createUser('tester', 'tester@example.org');
+        $this->runTest($client, 'tester@example.org');
+    }
+
+    private function runTest(KernelBrowser $client, string $query): void
+    {
+        $client->followRedirects();
+        $client->request('GET', '/forgot-password');
+        $client->submitForm('Search', [
+            'form[query]' => $query,
+        ]);
+
+        self::assertResponseIsSuccessful();
+        self::assertSelectorTextContains('p', 'Please check your inbox.');
+    }
+}
